e2c386841ac7f8f5ca213321ee3345ca494fdea6
Atm0027 <alextorresm78@gmail.com>
2026-02-05 20:23:32 +0100

    limpieza de css

--- public/js/config.env.js
+++ public/js/config.env.js
@@ -4,6 +4,6 @@
 // ========================================================
 window.CIVIS_ENV = {
     API_BASE_URL: "http://civis.local/api",
-    GENERATED_AT: "2026-02-05T18:23:20+00:00"
+    GENERATED_AT: "2026-02-05T19:03:23+00:00"
 };
 console.log('[CIVIS] API URL:', window.CIVIS_ENV.API_BASE_URL);
